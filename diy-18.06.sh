2be8f98af9509fa8653d59e8b2e9ca136299210b
Z-Prion <63301611+Z-Prion@users.noreply.github.com>
2021-12-27 19:34:42 +0800

    Initial commit

---
+++ diy-18.06.sh
@@ -0,0 +1,5 @@
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git kiddin9_packages https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
